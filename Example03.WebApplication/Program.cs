91ecaca4d90fcd1c7b345e0d7edf06396501e5c8
kazimgoksel.kalyoncu <kazimgoksel.kalyoncu@bilgeadamboost.com>
2024-04-17 17:15:35 +0300

    Tag helpers

--- Example03.WebApplication/Program.cs
+++ Example03.WebApplication/Program.cs
@@ -1,15 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
+//app.UseDeveloperExceptionPage();
 
-app.MapDefaultControllerRoute();
+//app.MapDefaultControllerRoute();
 //app.MapControllerRoute(
 //    name:"default",
 //    pattern:"{action=Index}/{controller=Home}/{id?}"
 //    );
+
+//app.MapGet("/", async (httpContext) =>
+//{
+//    //return "Ana sayfa <b>Test</b>";
+//    httpContext.Response.ContentType = "text/html";
+//    await httpContext.Response.WriteAsync("Ana sayfa <b>Test</b>");
+//});
 
 app.Run();
