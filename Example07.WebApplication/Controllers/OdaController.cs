b5818ed483c9c8356a776f6466d2cf54101a361e
kazimgoksel.kalyoncu <kazimgoksel.kalyoncu@bilgeadamboost.com>
2024-04-25 12:35:26 +0300

    ActionResult Tipleri, Partial View Ajax giriş

---
+++ Example07.WebApplication/Controllers/OdaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example07.WebApplication.Controllers
+{
+    public class OdaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormCollection form)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Example07.WebApplication/Controllers/OtelController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example07.WebApplication.Controllers
+{
+    public class OtelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(bool isPartial = false)
+        {
+            ViewBag.IsPartial = isPartial;
+            if (isPartial)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormCollection form)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
